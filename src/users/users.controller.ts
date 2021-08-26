04afee076dd5f2474227e7086f1ce8e54c0eab79
Aurélien Brabant <abrabant@student.42.fr>
2021-08-26 21:42:58 +0200

    Start to implement user creation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import {CreateUserDto} from './dto/create-user.dto';
+import {UsersService} from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(
+    private readonly usersService: UsersService
+  ) {
+  }
+
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+  
+  @Post()
+  create(@Body() userData: CreateUserDto) {
+    return this.usersService.create(userData);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,17 +1,22 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import {InjectRepository} from '@nestjs/typeorm';
 import {Repository} from 'typeorm';
+import {CreateUserDto} from './dto/create-user.dto';
 import {UserEntity} from './user.entity';
+
+const bcrypt = require('bcrypt');
 
 @Injectable()
 export class UsersService {
+  saltRounds = 10;
+
   constructor(
     @InjectRepository(UserEntity)
-    private readonly userRepository: Repository<UserEntity>
+    private readonly usersRepository: Repository<UserEntity>
   ) {}
 
   async findByEmail(email: string): Promise<UserEntity> {
-    const user = await this.userRepository.findOne({ email });
+    const user = await this.usersRepository.findOne({ email });
 
     if (!user) {
       throw new NotFoundException('No user with this email address');
@@ -19,5 +24,19 @@ export class UsersService {
 
     delete user.password;
     return user;
+  }
+
+  findAll(): Promise<UserEntity[]> {
+    return this.usersRepository.find();
+  }
+
+  async create(userData: CreateUserDto): Promise<UserEntity> {
+    userData.password = await bcrypt.hash(userData.password, this.saltRounds);
+
+    let newUser = this.usersRepository.create(userData);
+    newUser = await this.usersRepository.save(userData);
+
+    delete newUser.password;
+    return newUser; 
   }
 }
