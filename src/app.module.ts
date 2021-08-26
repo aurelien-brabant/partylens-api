0c21bcd59885b2163d01cf636ce0ed110e638efa
Aurélien Brabant <abrabant@student.42.fr>
2021-08-26 21:07:50 +0200

    Setup DB connection (postgres)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import {TypeOrmModule} from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      host: process.env.POSTGRES_HOST,
+      autoLoadEntities: true,
+      synchronize: process.env.NODE_ENV === 'development' /* synchronization must be always disabled in production! */
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
