b4a30a76f214b7ec2e4e5bac42bb9cf0e7993279
Aurélien Brabant <abrabant@student.42.fr>
2021-08-30 16:24:58 +0200

    Use hasPermission in guard instead of duplicating code

--- src/parties/guard/memberpermission.guard.ts
+++ src/parties/guard/memberpermission.guard.ts
@@ -1,6 +1,4 @@
-import {CanActivate, ExecutionContext, Injectable, mixin, UnauthorizedException} from "@nestjs/common";
-import {NotFoundError} from "rxjs";
-import {MPBit} from "../entity/partymember.entity";
+import {CanActivate, ExecutionContext, Injectable, mixin, NotFoundException, UnauthorizedException} from "@nestjs/common";
 import {PartymembersService} from "../service/partymembers.service";
 
 export const MemberPermissionGuard = (permissionBits: number) => {
@@ -18,24 +16,10 @@ export const MemberPermissionGuard = (permissionBits: number) => {
 
       /* should never happen in pratice */
       if (!loggedMember) {
-        throw new NotFoundError('Unexpected permission guard error: member not found');
+        throw new NotFoundException('Unexpected permission guard error: member not found');
       }
-
-      /* loop over each entry of the MPBit enum, converting each one into a number */
-      for (const pBitString in MPBit) {
-        const pBit = Number(pBitString);
- 
-        /**
-         * If the entry is present in permissionBits, it should be too in loggedMember's permission bits.
-         * In case it is not, the access is properly denied
-         */
-
-        if ((pBit & permissionBits) && !(loggedMember.permissionBits & pBit)) {
-          throw new UnauthorizedException('Could not access route: missing member permission(s).');
-        }
-      }
-
-      return true;
+      
+      return this.partymembersService.hasPermission(loggedMember, permissionBits);
     }
   }
 
