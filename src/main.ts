20ab4ead04da7e13fbfaa70620b07bf3bd73d96a
Aurélien Brabant <abrabant@student.42.fr>
2021-08-31 15:33:10 +0200

    Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   if (process.env.NODE_ENV === 'development') {
     const config = new DocumentBuilder()
     .setTitle('Partylens API')
