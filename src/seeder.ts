fcde2e23d0f4391e338a5dcd4a2ff1d19252f1df
Aurélien Brabant <abrabant@student.42.fr>
2021-08-31 09:20:03 +0200

    Add proper dev environment setup through docker and docker-compose

--- src/seeder.ts
+++ src/seeder.ts
@@ -1,17 +1,26 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import { SeedersModule } from "./seeders/seeders.module";
 import { SeedersService } from "./seeders/seeders.service";
+
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+  .option('-u, --user-nb <number>', 'number of random users that will be generated', '20')
+  .option('-p --party-nb <number>', 'number of random parties that will be generated', '5')
+  .option('-P --user-password <password>', 'The password which is used for every generated user except admin', 'pass')
+
+program.parse(process.argv)
+
+const options = program.opts();
 
 async function bootstrap() {
     NestFactory.createApplicationContext(AppModule)
       .then(appContext => {
         const seeder = appContext.get(SeedersService);
         seeder
-          .seed()
-          .then(() => {
-              console.log('seeding OK');
-          })
+          .seed(options.userNb, options.partyNb, options.userPassword)
           .catch(error => {
             throw error;
           })
